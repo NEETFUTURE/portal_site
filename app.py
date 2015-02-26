0b8322eda5a421a0de892811b9f26c03ff94228a
NEETFUTURE <marumarumaru62@gmail.com>
2015-02-26 23:47:51 +0900

    フォームメール実装 reCAPCHAはapp.pyでコメントアウト状態

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from contextlib import closing
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for
 from flask import abort, render_template, flash, send_from_directory, Response
+from flask.ext.mail import Mail
+from flask.ext.mail import Message
 import json
 from werkzeug import secure_filename
 import os
@@ -12,6 +14,8 @@ from datetime import datetime as dtime
 import traceback
 import sys
 from db_orm import ormer
+import urllib.request
+import urllib.parse
 
 CAROUSEL = 'carousel.db'
 HIGAWARI = 'higawari.db'
@@ -19,10 +23,31 @@ OPINION = 'opinion.db'
 OSIRASE = 'osirase.db'
 UPLOADDIR = "upload_picture"
 
+POR_MAIL = 'gakusyoku@gmail.com'
+POR_MAIL_PASS = 'password'
 
 
 app = Flask(__name__)
 app.config.from_object(__name__)
+app.config.update(dict(
+    DEBUG = True,
+    MAIL_SERVER = 'smtp.gmail.com',
+    MAIL_PORT = 587,
+    MAIL_USE_TLS = True,
+    MAIL_USE_SSL = False,
+    MAIL_USERNAME = POR_MAIL,
+    MAIL_PASSWORD = POR_MAIL_PASS,
+))
+mail = Mail(app)
+
+def sendmail(data):
+    msg = Message(data["title"],
+                  sender=data["sender"],
+                  recipients=data["recipients"])
+    msg.body = data["body"]
+    mail.send(msg)
+    return True
+
 
 @app.route("/")
 @app.route("/index")
@@ -63,7 +88,40 @@ def connect():
 
 @app.route("/opinion")
 def opinion():
-    return render_template("index.html")
+    return render_template("form.html")
+
+#reCAPCHAを使って認証＆メール送信
+@app.route("/sendopinion",methods=["POST"])
+def sendopinion():
+#    if(request.from["g-recaptcha-response"]==""):
+#        return "画像認証を受けてください"
+
+#    url="https://www.google.com/recaptcha/api/siteverify"
+#    keys={"secret":"しーくれっときー",
+#          "response":request.form["g-recaptcha-response"]}
+#    req=urllib.request.Request(url,keys)
+#    res=urllib.request.urlopen(req)
+#    d_data=json.loads(res.read().decode("utf-8"))
+#    if(d_data[success]==False):
+#        return d_data["error-codes"][1]+"\n"+d_data["error-codes"][2]
+
+
+    formdata = {"title" :"要望",
+                "sender":POR_MAIL,
+                "body"  :request.form["body"],
+                "recipients":[POR_MAIL]}
+    if(sendmail(formdata)==False):
+        return "メール送れなかったのん"
+
+    sendmail({"title" :"投稿を受け取りました",
+              "sender":"学食ポータル",
+              "body":"""
+              あなたのメールを承りました。
+              近日中にあなたの質問への解答が行きます。
+              もし解答が中々来ない場合でも、他の解答の状態によって変わりますのでお待ちください。
+              東京電機大学学生食堂ポータルサイト計画実行委員会""",
+              "recipients":[request.form["adress"]]})
+    return "送信成功"
 
 
 #ファイルへのリンクを返すルーティング
