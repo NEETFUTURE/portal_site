45fc44c4cc5fc888fcfbaacf30e42a97b7caa9ed
NEETFUTURE <marumarumaru62@gmail.com>
2015-03-27 22:22:16 +0900

    カルーセルを３つに増やした

--- carousel.sql
+++ carousel.sql
@@ -1,6 +1,6 @@
 drop table if exists carousel;
 create table carousel(
-	id integer primary key autoincrement,
+	id integer not null,
     pic_name string not null,
     h1_str string not null,
     link string not null
