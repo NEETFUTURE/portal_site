0ce7cb95e0f96ea25353edf995b7982b6e863ad8
tableamd <espoir204@softbank.ne.jp>
2015-05-02 19:12:05 +0900

    多分正常作動…？ 一応動いてるっぽいです。ログメッセージとかは出ないのでスッキリ

--- app.py
+++ app.py
@@ -1,37 +1,32 @@
 # -*- coding: utf_8 -*-
 
 from __future__ import with_statement
-from contextlib import closing
-import sqlite3
+#from contextlib import closing
+#import sqlite3
+#from werkzeug import secure_filename
+#from datetime import datetime as dtime
+#import traceback
+#from db_orm import ormer
 from flask import Flask, request, session, g, redirect, url_for
 from flask import abort, render_template, flash, send_from_directory, Response
 from flask.ext.mail import Mail
 from flask.ext.mail import Message
 import json
-from werkzeug import secure_filename
 import os
-from datetime import datetime as dtime
-import traceback
 import sys
-from db_orm import ormer
+from flask.ext.sqlalchemy import SQLAlchemy
 import urllib.request
 import urllib.parse
 
-CAROUSEL = 'carousel.db'
-HIGAWARI = 'higawari.db'
-OPINION = 'opinion.db'
-OSIRASE = 'osirase.db'
-UPLOADDIR = "upload_picture"
 SECRET_KEY = os.urandom(20)
-
-
+UPLOADDIR = "upload_picture"
 USERNAME = "admin@gmail.com"
 PASSWORD = "yuruyuriISgod"
 
 POR_MAIL = 'gakusyoku@gmail.com'
 POR_MAIL_PASS = 'hogehoge'
 
-
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.config.from_object(__name__)
@@ -44,7 +39,20 @@ app.config.update(dict(
     MAIL_USERNAME = POR_MAIL,
     MAIL_PASSWORD = POR_MAIL_PASS,
 ))
-mail = Mail(app)
+
+app.config["SQLALCHEMY_DATABASE_URI"] =\
+ "sqlite:///" + os.path.join(basedir,"data.sqlite")
+app.config["SQLALCHEMY_COMMIT_ON_TEARDOWN"] = True
+
+mail = Mail(app) #flask-mail
+db = SQLAlchemy(app) #flask-sqlalchemy
+
+
+#2回目以降のimportを防ぐ
+if not "ormer" in sys.modules.keys():
+    from db_orm.ormer2 import *
+else:
+    pass
 
 def sendmail(data):
     msg = Message(data["title"],
@@ -64,21 +72,21 @@ iden_list = ["default","a","b","c","d","e","f","r",
 
 # @app.before_request
 # def before_request():
-#     h = ormer.Higawari.return1st_by_id(id=1)
+#     h = Higawari.return1st_by_id(id=1)
 #     data = h.time
-#     data = ormer.changeStringToDatetime(data)
+#     data = changeStringToDatetime(data)
 #     now = dtime.now()
 #     if (now-data).days >= 1:
-#         h.time = ormer.changeDatetimeToString(now,3)
-#         ormer.Higawari.session.commit()
+#         h.time = changeDatetimeToString(now,3)
+#         db.session.commit()
 
 
 @app.route("/")
 @app.route("/index")
 def top_page():
     return render_template("top.html",
-                           carousel_list=ormer.Carousel.get_dict(),
-                           osirase_list=ormer.Osirase.getAllData())
+                           carousel_list=Carousel.get_dict(),
+                           osirase_list=Osirase.getAllData())
 
 
 @app.route("/higawari")
@@ -88,41 +96,20 @@ def higawari():
 
 @app.route("/rank")
 def rank():
-    # h = ormer.Higawari.return1st_by_id(id=1)
-    # time = h.time
-    # time = time.replace("/","_")
-    # menu_vote = []
-    # for m,v in zip(dir(h)[32:45],dir(h)[49:61]):
-    #     menu_vote.append([m,eval("h.%s"%m),eval("h.%s"%v)])
-
-    # menu_vote.sort(key=lambda x:x[2])
-
-    # return render_template("rank.html",time=time,menu_vote=menu_vote[::-1])
-
-    #today = ormer.changeDatetimeToString(dtime.now(),3)
-    today = "2015/3/23" #テスト用の日付
-    menu = ormer.Higawari2.return_desclist_by_date(today)
+    today = "2015/5/2" #テスト用の日付
+    menu = Higawari2.return_desclist_by_date(today)
 
     return render_template("rank.html",
                            time=today.replace("/","_"), 
                            menu=menu)
 
+
 @app.route("/connect", methods=["POST"])
 def connect():
-    # g = request.json
-    # h = ormer.Higawari.return1st_by_id(id=1)
-
-    # #exec使いたくないけどデータベースのカラム上これの方がシンプル。。。
-    # exec("""h.vote_%s+=1"""%g)
-    # ormer.Higawari.session.commit()
-
-    # #これまたevalも使いたくないんだけど(ry
-    # return Response(json.dumps(eval("h.vote_%s"%g)))
-
     js = request.json.split("*")
-    h = ormer.Higawari2.return_by_IdentandDate(js[0],js[1].replace("_","/"))
+    h = Higawari2.return_by_IdentandDate(js[0],js[1].replace("_","/"))
     h.vote += 1
-    ormer.Higawari2.session.commit()
+    db.session.commit()
 
     return Response(json.dumps(h.vote))
 
@@ -130,6 +117,7 @@ def connect():
 @app.route("/opinion")
 def opinion():
     return render_template("form.html")
+
 
 #reCAPCHAを使って認証＆メール送信
 @app.route("/sendopinion",methods=["POST"])
@@ -195,7 +183,7 @@ def adminpage():
     except:
         return redirect(url_for("top_page"))
 
-    car = ormer.Carousel.get_dict()
+    car = Carousel.get_dict()
 
     if "date" in request.form:
         today = request.form["date"]
@@ -204,10 +192,10 @@ def adminpage():
         pass
     else:
         #today = ormer.changeDatetimeToString(dtime.now(),3)
-        today = "2015/3/21" #テスト用の日付
-        dates = ormer.Bussday.hoge(today, 4)
+        today = "2015/5/2" #テスト用の日付
+        dates = Bussday.hoge(today, 4)
 
-    menu = ormer.Higawari2.return_desclist_by_date(today)
+    menu = Higawari2.return_desclist_by_date(today)
 
     files = os.listdir('static/img/')
 
@@ -215,7 +203,7 @@ def adminpage():
                            today=today,
                            carousel=car,
                            files=files,
-                           osirase=ormer.Osirase.getAllData(),
+                           osirase=Osirase.getAllData(),
                            ufiles=os.listdir(UPLOADDIR),
                            dates=dates,
                            menu=menu,
@@ -237,12 +225,10 @@ def change_higawari():
     if request.method == "POST":
 
         del_list = request.form.getlist("ch")
-        #print("*"*30)
-        #print(del_list)
         for iden in del_list:
-            h = ormer.Higawari2.return_by_IdentandDate(iden, request.form["today"])
-            ormer.Higawari2.session.delete(h)
-        ormer.Higawari2.session.commit()
+            h = Higawari2.return_by_IdentandDate(iden, request.form["today"])
+            db.session.delete(h)
+        db.session.commit()
 
         for i in range(0, 10):
             if request.form["s_"+str(i)] == "default" or request.form["s_"+str(i)] in del_list:
@@ -256,16 +242,16 @@ def change_higawari():
                     vote = int(request.form["v_"+str(i)])
                 except:
                     price = vote = 0
-                h = ormer.Higawari2.return_by_IdentandDate(ide, d)
+                h = Higawari2.return_by_IdentandDate(ide, d)
                 if h:
                     h.name = name
                     h.price = price
                     h.vote = vote
-                    ormer.Higawari2.session.commit()
+                    db.session.commit()
                 else:
-                    h = ormer.Higawari2(identify=ide,time=d,name=name,price=price)
-                    ormer.Higawari2.session.add(h)
-                    ormer.Higawari2.session.commit()
+                    h = Higawari2(identify=ide,time=d,name=name,price=price)
+                    db.session.add(h)
+                    db.session.commit()
         today = request.form["today"]
         prev = True
 
@@ -285,33 +271,35 @@ def change_carousel():
     #データベース更新
     i=1
     while "link_{0}".format(i) in request.form:
-        ormer.Carousel.updateLink(i,
-                                  request.form["h1_str_{0}".format(i)],
-                                  request.form["link_{0}".format(i)])
+        Carousel.updateLink(i,
+                            request.form["h1_str_{0}".format(i)],
+                            request.form["link_{0}".format(i)])
         i+=1
 
     return redirect(url_for("adminpage"))
 
+
 @app.route("/del_osirase",methods=["POST"])
 def del_osirase():
-    ormer.Osirase.delData(request.form['del'])
+    Osirase.delData(request.form['del'])
     return redirect(url_for("adminpage"))
+
 
 @app.route("/add_osirase",methods=["POST"])
 def add_osirase():
-    ormer.Osirase.addData(request.form['osi_title'],request.form['osi_link'])
+    Osirase.addData(request.form['osi_title'],request.form['osi_link'])
     return redirect(url_for("adminpage"))
+
 
 @app.route("/upload", methods=["POST"])
 def upload():
     if not session["login"]:
         return redirect(url_for("top_page"))
     upload_file = request.files.getlist("file[]")
-    #print("*"*20)
+
     for f in upload_file:
-        #print(f.filename)
         f.save(os.path.join(UPLOADDIR,f.filename))
-    #print("*"*20)
+
     return redirect(url_for("adminpage"))
 
 
---
+++ db_orm/ormer2.py
@@ -0,0 +1,210 @@
+#coding:utf-8
+from app import db
+from sqlalchemy import desc, asc
+import datetime
+
+iden_list_2 = ["a","b","c","d","e","f","r"]
+iden_list_m2 = ["pa","udo","soba","ra","rb"]
+
+def changeStringToDatetime(str_date):
+    """
+    strのフォーマットはyear/month/day/hour/minute/second
+    """
+    a = str_date.split("/")
+    a = list(map(int,a))
+    return datetime.datetime(*a)
+
+
+def changeDatetimeToString(date,num):
+    y = date.year
+    m = date.month
+    d = date.day
+    h = date.hour
+    mi = date.minute
+    s = date.second
+
+    if num == 3:
+        return "%s/%s/%s"%(y,m,d)
+    else:
+        return "%s/%s/%s/%s/%s/%s"%(y,m,d,h,mi,s)
+
+
+class Carousel(db.Model):
+    __tablename__ = "carousel"
+    id = db.Column(db.Integer, primary_key=True)
+    pic_name = db.Column(db.String())
+    h1_str = db.Column(db.String())
+    link = db.Column(db.String())
+
+    def __init__(self,pic_name,h1_str,link):
+        self.pic_name = pic_name
+        self.h1_str = h1_str
+        self.link = link
+
+    def __repr__(self):
+        return "<Carousel %r>"%self.pic_name
+
+    #Carouselテーブルの内容をすべて辞書にまとめて返す
+    @classmethod
+    def get_dict(cls):
+        l = []
+        for i in cls.query.all():
+            l.append(dict(id=i.id,link=i.link,h1_str=i.h1_str))
+
+        return l
+
+    #IDで指定したレコードを削除
+    @classmethod
+    def delById(cls, id):
+        target = cls.query.filter_by(id=id).first()
+        db.session.delete(target)
+        db.session.commit()
+
+    #IDで指定したカラムの文字列とリンクを更新
+    def updateLink(cls, id, h1_str, link):
+        target = cls.query.first_by(id=id).first()
+        target.h1_str = h1_str
+        target.link = link
+        db.session.commit()
+
+
+class Higawari2(db.Model):
+    __tablename__ = "higawari2"
+    id = db.Column(db.Integer, primary_key=True)
+    time = db.Column(db.String())
+    name = db.Column(db.String())
+    identify = db.Column(db.String())
+    price = db.Column(db.Integer)
+    vote = db.Column(db.Integer, default=0)
+
+    def __init__(self, time, name, identify, price):
+        self.time = time
+        self.name = name
+        self.identify = identify
+        self.price = price
+
+    def __repr__(self):
+        return "<Higawari2 %r>"%self.name
+
+    @classmethod
+    def return_desclist_by_date(cls, d_str):
+        l = []
+        for h in cls.query.order_by(desc(cls.vote)).filter_by(time=d_str):
+            l.append(dict(name=h.name,
+                          identify=h.identify,
+                          price=h.price,
+                          vote=h.vote))
+
+        return l
+
+    @classmethod
+    def return_1st_by_identify(cls, identify):
+        return cls.query.filter_by(identify=identify).first()
+
+    @classmethod
+    def return_by_IdentandDate(cls, identify, str_date):
+        return cls.query.filter_by(identify=identify,time=str_date).first()
+
+    @classmethod
+    def hogehoge(cls, str_date):
+        gaku_2 = []
+        gaku_m2 = []
+
+        for name in iden_list_2:
+            try:
+                gaku_2.append(cls.return_by_IdentandDate(name, str_date).name)
+            except:
+                pass
+
+        for name in iden_list_m2:
+            try:
+                gaku_m2.append(cls.return_by_IdentandDate(name, str_date).name)
+            except:
+                pass
+
+        return gaku_2,gaku_m2
+
+
+class Bussday(db.Model):
+    __tablename__ = "bussday"
+    id = db.Column(db.Integer, primary_key=True)
+    time = db.Column(db.String())
+
+    def __init__(self, time):
+        self.time = time
+
+    def __repr__(self):
+        return "<Bussday %r>"%self.time
+
+    @classmethod
+    def hoge(cls, now, num):
+        dates = []
+        i = cls.query.filter_by(time=now).first().id
+
+        for val in range(i, num+1):
+            d = cls.query.filter_by(id=val).first().time
+            dates.append(d)
+
+        return dates
+
+
+class Osirase(db.Model):
+    __tablename__ = "osirase"
+    id = db.Column(db.Integer, primary_key=True)
+    time = db.Column(db.String())
+    title = db.Column(db.String())
+    link = db.Column(db.String())
+
+    def __init__(self, title, time, link):
+        self.title = title
+        self.link = link
+        self.time = time
+
+    def __repr__(self):
+        return "<Osirase %r>"%self.title
+
+    @classmethod
+    def delData(cls, id):
+        target = cls.query.filter_by(id=id).first()
+        db.session.delet(target)
+        db.session.commit()
+
+    @classmethod
+    def getData(cls, id):
+        return cls.query.filter_by(id=id).first()
+
+    @classmethod
+    def getAllData(cls):
+        return cls.query.all()
+
+    @classmethod
+    def setData(cls, title, link):
+        target = cls.query.filter_by(id=id).first()
+        target.time = datetime.date.today().strftime("%Y/%m/%d")
+        target.link = link
+        db.session.commit()
+
+    @classmethod
+    def addData(cls, title, link):
+        ob = Osirase(title=title,
+                     link=link,
+                     time=datetime.date.today().strftime("%Y/%m/%d"))
+        db.session.add(ob)
+        db.session.commit()
+
+
+class Opinion(db.Model):
+    __tablename__ = "opinion"
+    id = db.Column(db.Integer, primary_key=True)
+    question = db.Column(db.String())
+    answer = db.Column(db.String())
+    time = db.Column(db.String())
+
+    def __init__(self, question, answer, time):
+        self.question = question
+        self.answer = answer
+        self.time = time
+
+    def __repr__(self):
+        return "<Opinion %r>"%self.question
+
